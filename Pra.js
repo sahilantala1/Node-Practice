f389e77cd2b31d9da7bcc59043bd7759685638b4
sahilantala1 <sahilantala16@gmail.com>
2024-03-18 16:17:26 +0530

    Node Practice

---
+++ Pra.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.mkdirSync("New Folder");
+// fs.writeFileSync("New Folder/bio.txt", "Hello");
+
+// fs.appendFileSync("New Folder/bio.txt", "Hii");
+
+// fs.renameSync("New Folder/bio.txt", "New Folder/mybio.txt");
+
+// fs.unlinkSync("New Folder/mybio.txt");
+// fs.rmdirSync("New Folder");
