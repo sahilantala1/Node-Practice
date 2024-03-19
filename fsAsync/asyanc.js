d1185f6c99828fe0edd7b3701fb3526b9295e9c5
sahilantala1 <sahilantala16@gmail.com>
2024-03-19 11:02:00 +0530

    add asyanc.js file

---
+++ fsAsync/asyanc.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+// fs.writeFile("add.txt", "Hello MF", (err) => {
+//   console.log(err);
+// });
+
+// fs.readFile("add.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+// fs.appendFile("add.txt", "never", (err) => {
+//   console.log(err);
+// });
+
+// fs.mkdir("newFolder", (err) => {
+//   console.log("Created");
+// });
+
+// fs.writeFile("newFolder/bio.txt", "Heoo", (err) => {
+//   console.log(err);
+// });
+
+// fs.appendFile("newFolder/bio.txt", "Last", (err) => {
+//   console.log(err);
+// });
+
+// fs.readFile("newFolder/bio.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+// fs.rename("newFolder/bio.txt", "newFolder/mybio.txt", (err) => {
+//   console.log(err);
+// });
+
+// fs.unlink("newFolder/mybio.txt", (err) => {
+//   console.log(err);
+// });
+
+// fs.rmdir("newFolder", (err) => {
+//   console.log(err);
+// });
+
+const os = require("os");
+
+const freememomry = os.freemem();
+
+console.log(`${freememomry / 1024 / 1024 / 1024 / 1024}`);
