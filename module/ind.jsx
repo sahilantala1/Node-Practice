654fab61ddf42465865462e0e633be988fc4dfb6
sahilantala1 <sahilantala16@gmail.com>
2024-03-20 14:44:05 +0530

    add npm

---
+++ module/ind.jsx
@@ -0,0 +1,4 @@
+const { add, sub } = require("./ope.jsx");
+
+console.log(add(5, 5));
+console.log(sub(5, 5));
---
+++ npm/index.js
@@ -0,0 +1,7 @@
+// const chalk = require("chalk");
+
+var validator = require("validator");
+
+const ans = validator.isEmail("foo@bar.com");
+console.log(ans);
+console.log(ans);
